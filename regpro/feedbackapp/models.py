a9b85bd784558b43d6392a810214cff3944b9a29
gibinbiju <gibinbiju45@gmail.com>
2019-11-06 16:05:03 +0530

    Regapp

---
+++ regpro/feedbackapp/models.py
@@ -0,0 +1,15 @@
+# from django.db import models
+#
+# # Create your models here.
+#
+# class Course(models.Model):
+#     coursename=models.CharField(max_length=128)
+#
+#     def __str__(self):
+#         return self.coursename
+#
+# class Batch(models.Model):
+#     batchcode=models.CharField(max_length=25)
+#     coursename=models.ForeignKey(Course,on_delete=models.CASCADE)
+#
+#     def __
